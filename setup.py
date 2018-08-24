2cad106f818d5b4ba0c9309de1f72969a0bbcbe2
Enea Ceolini <enea.ceolini@gmail.com>
2018-08-24 11:07:02 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='cochlea_utils',
+      version='0.1',
+      description='Utilities for cochlea events reading, localization and separation',
+      url='https://github.com/SensorsAudioINI/cochlea_utils',
+      author='Jithendar Anumula (UZH/ETH Zurich)',
+      author_email='anumulaj@ini.uzh.ch',
+      license='MIT',
+      packages=['cochlea_utils'],
+      install_requires=[
+            'scipy',
+            'matplotlib',
+            'progressbar'
+      ],
+      extras_require={
+      },
+      zip_safe=False,
+      )
