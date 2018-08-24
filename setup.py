bef4186ecfb0c6e5b99a83c2cf50766d1731096f
Enea Ceolini <enea.ceolini@gmail.com>
2018-08-24 11:25:30 +0200

    changed name of package and fixed installation

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='cochlea_utils',
+setup(name='cochelp',
       version='0.1',
       description='Utilities for cochlea events reading, localization and separation',
       url='https://github.com/SensorsAudioINI/cochlea_utils',
       author='Jithendar Anumula (UZH/ETH Zurich)',
       author_email='anumulaj@ini.uzh.ch',
       license='MIT',
-      packages=['cochlea_utils'],
+      packages=['cochelp'],
       install_requires=[
             'scipy',
             'matplotlib',
